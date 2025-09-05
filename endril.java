634d352455f0ad6087fc5581ca3b73452f16c839
Endril Thiago <105693005+Endril18@users.noreply.github.com>
2025-09-05 11:10:50 -0300

    Atualizar o endril.java

--- endril.java
+++ endril.java
@@ -1,39 +1,5 @@
-
-public class usuario {
-
-    private String nome;
-    private String cidade;
-
-    public usuario(String nome, String regiao) {
-        this.nome = nome;
-        this.regiao = região;
-    }
-
-    // Getters e Setters para acessar e modificar os atributos privados
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRegiao() {
-        return regiao;
-    }
-
-    public void setRegiao(String regiao) {
-        this.regiao = regiao;
-    }
-
-    /**
-     * Método para exibir as informações.
-     */
-
-    public void exibirInformacoes() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Região: " + regiao);
-    }
-
-    
+public class Endril {
+  public static void main(String[] args){
+    System.out.println("Engenharia de Software 2025 - Endril);
+  }
+}
