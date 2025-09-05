674591d20a1df506b83ef899f8a185139b6f4de8
Isabelly Dias <114939436+isabellydiasf@users.noreply.github.com>
2025-09-05 10:24:31 -0300

    uma mensagem clara sobre qual alteração foi feita

--- App.java
+++ App.java
@@ -1,5 +1,10 @@
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("editado por isa");
+    falar();
+  }
+
+  public static void falar(){
+    System.out.println("uma alteração foi feita");
   }
 }
